4340fe0638210520ec3ec6b7d6cc1b908b3a2b2c
bramuel <jorammanoah1@gmail.com>
2021-02-19 23:53:21 -0800

    Rebuild

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import './bootstrap/css/bootstrap.min.css'
-import Dashboard from "./Dashboard";
-import Nav from "./Nav";
-import About from "./About";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Details from './Details'
-import Header from './Components/Header'
-import Main from './Components/Main'
-import Section from './Components/Section'
+
 import NavBar from './Components/NavBar'
-import Courses from './Components/Courses'
 import NavHeader from "./Components/NavHeader";
 
 
